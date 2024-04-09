48f1e7f53cf892dd33b57a4de0fff5d1d3fbfc54
Mateo Galache Guillot <mateogalache12@gmail.com>
2024-04-09 16:35:48 +0200

    redes sociales

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,9 +5,9 @@
             ¡Síguenos!
         </h4>
         <div class="redes">
-            <img src="{{ asset('images/instagram.png') }}" alt="Instagram" class="yt">
-            <img src="{{ asset('images/twitter.png') }}" alt="Twitter">
-            <img src="{{ asset('images/youtube.png') }}" alt="Youtube">
+            <a href="https://www.instagram.com/nyabot_oficial/"><img src="{{ asset('images/instagram.png') }}" alt="Instagram" class="yt"></a>
+            <a href="https://twitter.com/NyaBot799186"><img src="{{ asset('images/twitter.png') }}" alt="Twitter"></a>
+            <a href="https://www.youtube.com/@nya-bot"><img src="{{ asset('images/youtube.png') }}" alt="Youtube"></a>
         </div>
     </div>
 </div>
