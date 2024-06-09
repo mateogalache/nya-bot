f7749fd310009d6b76ae63cbbcd25e44c6c40e89
mateo.galache <mateo.galache12@gmail.com>
2024-06-09 20:09:08 +0200

    doc

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -209,6 +209,10 @@
             @endif
             @if($num==8)
                 <h3>{{$index[$num]}}</h3>
+                <br>
+                <div class="dDoc">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/28BdBZ2qiK8?si=hiHsA_BBsElv-mPr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
             @endif
             @if($num==9)
                 <h3>{{$index[$num]}}</h3>
